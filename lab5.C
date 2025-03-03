b0212a7f048d9c19191734ed8e787e884eda4b83
EthanC42 <ethan.g.crouch-1@ou.edu>
2025-03-02 18:17:51 -0600

    proportional ting

--- lab5.C
+++ lab5.C
@@ -1,15 +1,35 @@
 #include <kipr/wombat.h>
 #include <time.h>
 
-int distance, error;
+int distance, error, control;
+driveLeftWheel = 120;
+driveRightWheel = 120;
+programstate = 0;
+maxstate = 0;
 Kp = 0.5;
+
 int main()
 {
-
-    while (1)
+    camera_open();
+    while (program_state > maxstate)
     {
-        distance = analog(0);
-        error = 18 - distance;
-        create_drive_direct(Kp * error, Kp * error);
+        camera_update();
+        distance = get_object_center_x(0, 0);
+        error = 80 - distance;
+        control = Kp * error;
+
+        if (control > 1 || control < -1)
+        {
+            drive(control);
+        }
+        else
+        {
+            create_drive_direct(driveLeftWheel, driveRightWheel);
+        }
     }
+}
+
+void drive(int control)
+{
+    create_drive_direct(driveLeftWheel - control, driveRightWheel + control); // for right turn, input negative number
 }
