dfda0560f76df92abfcb6d05b4ac6e725ff9c154
Tristen Brown <tristen.brown@ou.edu>
2025-03-10 15:29:17 -0500

    Lab 6 Done

--- lab6PID.C
+++ lab6PID.C
@@ -7,6 +7,10 @@ int driveRightWheel = 120;
 int programstate = 0;
 int maxstate = 4; //Search, Go to Red, search, Go to green, end
 float Kp = 0.5;
+float Ki = 0.02;
+float Kd = 0.1;
+float dt = 0.05;
+float MAX_INTEGRAL = 1000;
 int redBox = 0; //Channel for red box
 int redThreshold = 120; //Threshold bounding box width for red box.
 int greenBucket = 1; // Channel for green bucket
@@ -23,23 +27,33 @@ int main()
     {
         create_stop();
         camera_update();
-        
-        if(get_object_count(0) > 0){
-         	printf("Red Box Found\n");
-            programstate++;
-            continue;
+        int count = get_object_count(redBox);
+        if(count > 0){
+            int i = 0;
+            for(i = 0; i < count; i++){
+				if(get_object_bbox(redBox, i).width > 10){
+					printf("Yellow Box Found\n");
+            		programstate++;
+            		break;
+                }
+            }
+
         }
-        else{
+        if(programstate < 1){
         	turn();
             msleep(50);
         }
     }
+    
+    int integral = 0;
+    int derivative = 0;
+    int previous_error = 0;
     while (programstate < 2)
     {
         camera_update();
-        if(get_object_bbox(redBox, 0).width > 120){
+        if(get_object_bbox(redBox, 0).width > 90){
             create_stop();
-			printf("Red Box Reached\n");
+			printf("Yellow Box Reached\n");
             msleep(2000);
             programstate++;
             continue;
@@ -48,7 +62,11 @@ int main()
         
         distance = get_object_center_x(redBox, 0);
         error = 80 - distance;
-        control = Kp * error;
+        integral += error * dt;
+        derivative = (error - previous_error) / dt;
+        if(integral > MAX_INTEGRAL){integral = MAX_INTEGRAL;}
+        if(integral < -MAX_INTEGRAL){integral = -MAX_INTEGRAL;}
+        control = Kp * error + Ki * integral + Kd * derivative;
 
         if (control > 1 || control < -1)
         {
@@ -65,18 +83,25 @@ int main()
     {
     	create_stop();
         camera_update();
-        
-        if(get_object_count(greenBucket) > 0){
-         	printf("Green Bucket Found\n");
-            programstate++;
-            continue;
+        int count = get_object_count(greenBucket);
+        if(count > 0){
+            int i = 0;
+            for(i = 0; i < count; i++){
+				if(get_object_bbox(greenBucket, i).width > 10){
+					printf("Green Box Found\n");
+            		programstate++;
+            		break;
+                }
+            }
         }
-        else{
+        if(programstate < 3){
         	turn();
             msleep(50);
         } 
     }
-    
+    integral = 0;
+    derivative = 0;
+    previous_error = 0;
     while(programstate < maxstate){
         camera_update();
         if(get_object_bbox(greenBucket, 0).width > 120){
@@ -89,7 +114,11 @@ int main()
         
         distance = get_object_center_x(greenBucket, 0);
         error = 80 - distance;
-        control = Kp * error;
+        integral += error * dt;
+        derivative = (error - previous_error) / dt;
+        if(integral > MAX_INTEGRAL){integral = MAX_INTEGRAL;}
+        if(integral < -MAX_INTEGRAL){integral = -MAX_INTEGRAL;}
+        control = Kp * error + Ki * integral + Kd * derivative;
 
         if (control > 1 || control < -1)
         {
