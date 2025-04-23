d8d1730e7197b148b431bdedaa462f2dc55aa753
EthanC42 <ethan.g.crouch-1@ou.edu>
2025-04-23 12:51:21 -0500

    Update lab11.c
    kermit de forg

--- lab11.c
+++ lab11.c
@@ -5,27 +5,27 @@ float Kp = 0.2;
 float Ki = 0.1;
 float Kd = 0.05;
 
-//safety variables
+// safety variables
 float MaxIntegral = 1000;
 
-//camera variables (PLACEHOLDER ATM)
-int greenBucket = 0; //channel for green bucket
-int redBucket = 0; //channel for red bucket
-int blueBucket = 0; //channel for blue bucket
-int greenThreshold = 0; //placeholder threshold
-int redThreshold = 0; //placeholder threshold
-int blueThreshold = 0; //placeholder threshold
+// camera variables (PLACEHOLDER ATM)
+int greenBucket = 0;    // channel for green bucket
+int redBucket = 0;      // channel for red bucket
+int blueBucket = 0;     // channel for blue bucket
+int greenThreshold = 0; // placeholder threshold
+int redThreshold = 0;   // placeholder threshold
+int blueThreshold = 0;  // placeholder threshold
 
-//Sensor port  (PLACEHOLDER ATM) MAY NOT BE NEEDED
+// Sensor port  (PLACEHOLDER ATM) MAY NOT BE NEEDED
 int sensor_left_port = 0;
 int sensor_right_port = 1;
 int sensor_center_port = 2;
 
-//motor speed base
+// motor speed base
 int driveLeftWheel = 120;
 int driveRightWheel = 120;
 
-//PID Variables
+// PID Variables
 float error, previous_error = 0, integral = 0, derivative;
 
 void turn();
@@ -33,44 +33,51 @@ void drive();
 void returnToRectangle();
 void findBorder();
 
-int main(){
+int main()
+{
     printf("connecting to create wallaby...\n");
     create_connect();
     printf("connected!");
     printf("connecting camera!");
     camera_open();
     printf("connected!");
-    int programState = 0; //used for program progression
-    //state 1:: find rectangle border
-    while (programState < 1){  
-        //find the rectangle border and align 
-       findBorder();
+    int programState = 0; // used for program progression
+    // state 1:: find rectangle border
+    while (programState < 1)
+    {
+        // find the rectangle border and align
+        findBorder();
     }
-    //begin the wallride
-    while (programState < 2){
-
+    // begin the wallride
+    while (programState < 2)
+    {
+        wallride();
+        if
     }
-    //find the first box
-    while(programState < 3){
-
+    // find the first box
+    while (programState < 3)
+    {
+        
     }
-    //return to rectangle
-    while(programState<4){
+    // return to rectangle
+    while (programState < 4)
+    {
         returnToRectangle();
         findBorder();
     }
-    //find the second box
-    while(programState<5){
-
+    // find the second box
+    while (programState < 5)
+    {
     }
-    //return to rectangle
-    while(programState<6){
+    // return to rectangle
+    while (programState < 6)
+    {
         returnToRectangle();
         findBorder();
     }
-    //find the third box
-    while(programState<7){
-
+    // find the third box
+    while (programState < 7)
+    {
     }
     printf("disconnecting...\n");
     create_stop();
@@ -79,54 +86,62 @@ int main(){
     return 0;
 }
 
-void drive{
+void drive
+{
     create_drive_direct(driveLeftWheel - control, driveRightWheel - control);
 }
 
-void turn{
+void turn()
+{
     create_drive_direct(driveLeftWheel, -driveRightWheel); // Turns right
 }
 
-void findBorder{
-      //while loop reading when we reach border [mind you example code here]
-      while([analogSensor] < THRESHOLD_FOR_WHITETAPE){
+void findBorder()
+{
+    // while loop reading when we reach border [mind you example code here]
+    while ([analogSensor] < THRESHOLD_FOR_WHITETAPE)
+    {
         drive();
     }
-    //stop when we reach the border
+    // stop when we reach the border
     create_stop();
 }
 
-void returnToRectangle{
-    create_drive_direct(-driveLeftWheel*2, -driveRightWheel*2); // goes backwards back into circle
-    msleep(1000); //sleep with drive command to return to rectangle
-    create_drive_direct(0, 0); //turn off motors post return
+void returnToRectangle()
+{
+    create_drive_direct(-driveLeftWheel * 2, -driveRightWheel * 2); // goes backwards back into circle
+    msleep(1000);                                                   // sleep with drive command to return to rectangle
+    create_drive_direct(0, 0);                                      // turn off motors post return
 }
 
-void wallRide{
-while(cameraMatch != 1){
-   
-    int time = (seconds() - starttime);
-    int lfCliff = get_create_lfcliff_amt();
-    int rfCliff = get_create_rfcliff_amt();
-    int lCliff = get_create_lcliff_amt();
-    int rCliff = get_create_rcliff_amt();
-    int error = lfCliff - rfCliff;
-    integral += error;
-    if (integral > MAX_INTEGRAL)
-        integral = MAX_INTEGRAL;
-    if (integral < -MAX_INTEGRAL)
-        integral = -MAX_INTEGRAL;
-    int derivative = error - previousError;
-    previousError = error;
-    int control = (int)(Kp * error + Ki * integral + Kd * derivative);
+void wallRide()
+{
+    while (cameraMatch != 1)
+    { //(cameramatch: if blob is big 0_0
 
-   cameraMatch = 0; //reset camera match 
-}
-void cameraMatch(programState){
-    //basically a function to see if the robot see's a box
-    while(programState > 0){
+        int time = (seconds() - starttime);
+        int lfCliff = get_create_lfcliff_amt();
+        int rfCliff = get_create_rfcliff_amt();
+        int lCliff = get_create_lcliff_amt();
+        int rCliff = get_create_rcliff_amt();
+        int error = lfCliff - rfCliff;
+        integral += error;
+        if (integral > MAX_INTEGRAL)
+            integral = MAX_INTEGRAL;
+        if (integral < -MAX_INTEGRAL)
+            integral = -MAX_INTEGRAL;
+        int derivative = error - previousError;
+        previousError = error;
+        int control = (int)(Kp * error + Ki * integral + Kd * derivative);
 
+        cameraMatch = 0; // reset camera match
     }
 }
 
+void cameraMatch(programState)
+{
+    // basically a function to see if the robot see's a box
+    while (programState > 0)
+    {
+    }
 }
